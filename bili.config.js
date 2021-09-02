7b795c62d9bcb4c1ae4edc2b9238d153d7a2b71c
dima_z <dimazvazhii@gmail.com>
2021-09-02 13:50:45 +0300

    vue-webdatarocks created

---
+++ bili.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  banner: true,
+  externals: ['vue', 'webdatarocks'],
+  output: {
+    extractCSS: false,
+    format: [
+      "cjs", 
+      "es", 
+      "esm", 
+      "umd", 
+      "iife", 
+      "amd", 
+      "system", 
+      "cjs-min", 
+      "es-min", 
+      "esm-min", 
+      "umd-min", 
+      "iife-min", 
+      "amd-min", 
+      "system-min"
+    ],
+    moduleName: 'Pivot'
+  },
+  plugins: {
+    vue: {
+      css: true
+    }
+  }
+};
